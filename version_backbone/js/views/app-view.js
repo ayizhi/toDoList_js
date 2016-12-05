af6d97a9e98f817e4c583b356c32fb41ff5c9ef7
张奕智 <zhangyiizhi@126.com>
2016-12-05 15:04:07 +0800

    ifx

--- version_backbone/js/views/app-view.js
+++ version_backbone/js/views/app-view.js
@@ -15,7 +15,8 @@ var app = app || {};
 			var t = this;
 			t.allCheckbox = t.$('.toggle-all')[0];
 			t.$input = t.$('.new-todo');
-			t.$footer = t.$('.main');
+			t.$footer = this.$('.footer');
+			t.$main = this.$('.main');
 			t.$list = $('.todo-list');
 
 			t.listenTo(app.todos,'add',t.addOne);
@@ -26,6 +27,72 @@ var app = app || {};
 
 			app.todos.fetch({reset: true});
 		},
+
+		render: function(){
+			var completed = app.todos.completed().length;
+			var remaining = app.todos.remaining().length;
+
+			if(app.todos.length){
+				this.$main.show();
+				this.$footer.show();
+
+				this.$footer.html(this.statsTemplate({
+					completed: completed,
+					remaining: remaining,
+				}));
+
+				this.$('.filters li a')
+					.removeClass('selected')
+					.filter('[href="#/' + (app.TodoFilter || '') + '"]')
+					.addClass('selected');
+			}else{
+				this.$main.hide();
+				this.$footer.hide();
+			}
+			this.allCheckbox.checked = !remaining
+		},
+
+		addOne: function(todo){
+			var view = new app.TodoView({model: todo});
+			this.$list.append(view.render().el);
+		},
+
+		addAll: function(){
+			this.$list.html('');
+			app.todos.each(this.addOne,this);
+		},
+
+		filterOne: function(todo){
+			todo.trigger('visible');
+		},
+
+		filterAll: function(){
+			app.todos.each(this.filterOne,this)
+		},
+
+		newAttributes: function(){
+			return {
+				title: this.$input.val().trim(),
+				order: app.todos.nextOrder(),
+				completed: false
+			};
+		},
+
+		createOnEnter: function(e){
+			if(e.which === ENTER_KEY && this.$input.val().trim()){
+				app.todos.create(this.newAttributes());
+				this.$input.val('');
+			}
+		},
+
+		toggleAllComplete: function(){
+			var completed = this.allCheckbox.checked;
+			app.todos.each(function(todo){
+				todo.save({
+					completed: completed
+				})
+			})
+		}
 	})
 
 })($)
