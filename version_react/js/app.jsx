2f2f494680df1105ec5304ce8c12b8577e24a2b5
张奕智 <zhangyiizhi@126.com>
2016-12-06 23:44:08 +0800

    fix

--- version_react/js/app.jsx
+++ version_react/js/app.jsx
@@ -36,8 +36,50 @@ var app = app || {};
 			if(event.keyCode != ENTER_KEY){
 				return;
 			}
-			
-		}
+
+			event.preventDefault();
+
+			var val = this.state.newTodo.trim();
+
+			if(val){
+				this.props.model.addTodo(val);
+				this.setState({newTodo:''});
+			}
+		},
+
+		toggleAll: function(event){
+			var checked = event.target.checked;
+			this.props.model.toggleAll(checked);
+		},
+
+		toggle: function(todoToToggle){
+			this.props.model.toggle(todoToToggle)
+		},
+
+		destroy: function(todo){
+			this.props.model.destroy(todo);
+		},
+
+		edit: function(todo){
+			this.setState({editing: todo.id});
+		},
+
+		save: function(todoToSave,text){
+			this.props.model.save(todoToSave,text);
+			this.setState({editing: null});
+		},
+
+		cancel: function(){
+			this.setState({editing: null});
+		},
+
+		clearCompleted: function(){
+			this.props.model.clearCompleted();
+		},
+
+		render: function(){}
+
+
 	})
 
 })()
