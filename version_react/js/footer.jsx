bb7b043fff1127e15ff9321946e9ee38c2425354
张奕智 <zhangyiizhi@126.com>
2016-12-13 00:26:25 +0800

    fx

--- version_react/js/footer.jsx
+++ version_react/js/footer.jsx
@@ -4,7 +4,43 @@ var app = app || {};
 	'use strict';
 	app.TodoFooter = React.createClass({
 		render: function(){
-			
+			var activeTodoWord = app.Utils.pluralize(this.props.count,'item');
+			var clearButton = null;
+			if(this.props.completedCount > 0){
+				clearButton = (
+					<button className="clear-completed"
+							onClick={this.props.onClearCompleted}>
+							Clear completed
+					</button>
+				);
+			}
+
+			var nowShowing = this.props.nowShowing;
+			return (
+				<footer className="footer">
+					<span className="todo-count">
+						<strong>{this.props.count}</strong> {activeTodoWord} left
+					</span>
+					<ul className="filter">
+						<li>
+						<a href="#/" className={className({selected: nowShowing === app.ALL_TODOS})}>All</a>
+						</li>
+						{' '}
+						<li>
+						<a href="#/active" className={classNames({selected: nowShowing === app.ACTIVE_TODOS})}>Active</a>
+						</li>
+						{' '}
+						<li>
+						<a href="#/completed" className={classNames({selected: nowShowing === app.COMPLETED_TODOS})}>
+							Completed
+						</a>
+						</li>
+					</ul>
+					{clearButton}
+
+				</footer>
+			)
+
 		}
 	})
 })()
--- version_react/js/todoItem.jsx
+++ version_react/js/todoItem.jsx
@@ -48,23 +48,39 @@ var app = app || {}
 			);
 		},
 
-		componentDidUpdate: function (prevProps) {
-			if (!prevProps.editing && this.props.editing) {
-				var node = React.findDOMNode(this.refs.editField);
-				node.focus();
-				node.setSelectionRange(node.value.length, node.value.length);
-			}
-		},
-
 		componentDidUpdate: function(prevProps){
 			if(!prevProps.editing && this.props.editing){
 				var node = React.findDOMNode(this.refs.editField);
 				node.focus();
 				node.setSelectionRange(node.value.length, node.value.length);
 			}
-		}
+		},
 
-
+		render: function(){
+			return (
+				<li className={classNames({
+					completed: this.props.todo.completed,
+					editing: this.props.editing})}>
+					<div className="view">
+						<input className="toggle"
+							type="checkbox"
+							checked={this.props.todo.completed}
+							onChange={this.props.onToggle}
+						/>
+						<label onDoubleClick={this.handleEdit}>
+							{this.props.todo.title}
+						</label>
+						<button className="destroy" onClick={this.props.onDestroy}/>
+					</div>
+					<input 	ref="editField"
+							className="edit"
+							value={this.state.editText}
+							onBlur={this.handleSubmit}
+							onChange={this.handleChange}
+							onKeyDown={this.handleKeyDown}/>
+				</li>
+			)
+		},
 	})
 
 })()
